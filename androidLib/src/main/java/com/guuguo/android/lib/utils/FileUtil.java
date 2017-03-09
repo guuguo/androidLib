411bef24506ebddc21ad98e4804063a981dfe6eb
guuguo <guuguo@qq.com>
2017-03-09 21:25:07 +0800

    fileUtil新增installApk

--- androidLib/src/main/java/com/guuguo/android/lib/utils/FileUtil.java
+++ androidLib/src/main/java/com/guuguo/android/lib/utils/FileUtil.java
@@ -1,9 +1,15 @@
 package com.guuguo.android.lib.utils;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
+import android.support.v4.content.FileProvider;
 import android.text.TextUtils;
 import android.util.Log;
+
+import com.guuguo.android.lib.BaseApplication;
 
 import java.io.File;
 
@@ -13,6 +19,29 @@ import java.io.File;
 
 public class FileUtil {
     // 外部存储是否已挂接
+
+    /**
+     * 安装 apk 文件
+     *
+     * @param apkFile
+     */
+    public static void installApk(File apkFile) {
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            Uri contentUri = FileProvider.getUriForFile(BaseApplication.getInstance(), AppUtil.getPackageName() + ".fileprovider", apkFile);
+            intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
+        } else {
+            intent.setDataAndType(Uri.fromFile(apkFile), "application/vnd.android.package-archive");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        }
+        if (BaseApplication.getInstance().getPackageManager().queryIntentActivities(intent, 0).size() > 0) {
+            BaseApplication.getInstance().startActivity(intent);
+        }
+    }
+
+
     public static boolean isExternalStorageMounted() {
         String sts = Environment.getExternalStorageState();
         return sts.equals(Environment.MEDIA_MOUNTED);
