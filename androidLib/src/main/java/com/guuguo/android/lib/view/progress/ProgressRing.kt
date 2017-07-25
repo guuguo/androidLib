8e83806544c3755b73adbac971a53e90551a6258
guuguo <guuguo@qq.com>
2017-07-25 16:58:46 +0800

    progressRing使用新方法

--- androidLib/src/main/java/com/guuguo/android/lib/view/progress/ProgressRing.kt
+++ androidLib/src/main/java/com/guuguo/android/lib/view/progress/ProgressRing.kt
@@ -2,14 +2,13 @@ package com.guuguo.android.lib.view.progress
 
 import android.content.Context
 import android.content.res.TypedArray
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.RectF
+import android.graphics.*
+import android.support.annotation.IntRange
 import android.util.AttributeSet
 import android.view.View
 import com.guuguo.android.R
 import com.guuguo.android.lib.extension.dpToPx
+import com.guuguo.android.lib.extension.log
 
 
 /**
@@ -17,16 +16,27 @@ import com.guuguo.android.lib.extension.dpToPx
  * 项目 androidLib
  */
 
-class ProgressRing(context: Context, attrs: AttributeSet, defStyleAttr: Int = 0) : View(context, attrs) {
-    constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0)
+class ProgressRing : View {
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        val attributes = context.theme.obtainStyledAttributes(attrs, R.styleable.ProgressRing, defStyleAttr, 0)
+        initByAttributes(attributes)
+        attributes.recycle()
+        initPaint()
+    }
 
-    /**
-     * id:&:R.color.
-     */
+
     private lateinit var mBgPaint: Paint
     private lateinit var mProgressPaint: Paint
     private var progressbarWidth: Int = 0
-    private var progress = 20
+    var progress = 20
+        set(@IntRange(from = 0, to = 100) value) {
+            currentProgress = progress
+            field = value
+            invalidate()
+        }
+
     private var startAngle = 0
     private var sweepAngle = 0
     private var bgEndColor = Color.GRAY
@@ -34,15 +44,9 @@ class ProgressRing(context: Context, attrs: AttributeSet, defStyleAttr: Int = 0)
     private var bgMidColor = Color.GRAY
     private var progressStartColor = Color.YELLOW
     private var progressEndColor = Color.BLUE
-    private var isAnimShow = false
+    private var isAnimShow = true
 
-    init {
-        val attributes = context.theme.obtainStyledAttributes(attrs, R.styleable.CircleProgressBar, defStyleAttr, 0)
-        initByAttributes(attributes)
-        attributes.recycle()
-        initPaint()
-    }
-
+    private var currentProgress = 0
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         var heightMeasureSpec = heightMeasureSpec
         val widthMode = View.MeasureSpec.getMode(widthMeasureSpec)
@@ -51,8 +55,8 @@ class ProgressRing(context: Context, attrs: AttributeSet, defStyleAttr: Int = 0)
         var height = View.MeasureSpec.getSize(heightMeasureSpec) - paddingLeft - paddingRight
 
         if (widthMode == View.MeasureSpec.EXACTLY && heightMode != View.MeasureSpec.EXACTLY) {
-            val minHeight = Math.min(Math.sin(Math.toRadians(startAngle.toDouble())), Math.sin(Math.toRadians((startAngle - sweepAngle).toDouble()))) * (width / 2)
-            height = (width / 2 - minHeight).toInt()
+            val minHeight = Math.min(-Math.sin(Math.toRadians(startAngle.toDouble())), -Math.sin(Math.toRadians((startAngle + sweepAngle).toDouble()))) * (width / 2)
+            height = (width / 2 + progressbarWidth  - minHeight).toInt()
             heightMeasureSpec = View.MeasureSpec.makeMeasureSpec(height,
                     View.MeasureSpec.EXACTLY)
         }
@@ -61,11 +65,11 @@ class ProgressRing(context: Context, attrs: AttributeSet, defStyleAttr: Int = 0)
     }
 
     protected fun initByAttributes(attributes: TypedArray) {
-        progressbarWidth = attributes.getInteger(R.styleable.ProgressRing_cr_progressbar_width, 5.dpToPx())
+        progressbarWidth = attributes.getDimensionPixelSize(R.styleable.ProgressRing_cr_progressbar_width, 5.dpToPx())
         progress = attributes.getInteger(R.styleable.ProgressRing_cr_progress, progress)
-        bgStartColor = attributes.getColor(R.styleable.ProgressRing_cr_background_bar_start_color, Color.GRAY)
+        bgStartColor = attributes.getColor(R.styleable.ProgressRing_cr_background_bar_start_color, Color.parseColor("#ffffff"))
         bgMidColor = attributes.getColor(R.styleable.ProgressRing_cr_background_bar_mid_color, Color.GRAY)
-        bgEndColor = attributes.getColor(R.styleable.ProgressRing_cr_background_bar_end_color, Color.GRAY)
+        bgEndColor = attributes.getColor(R.styleable.ProgressRing_cr_background_bar_end_color, Color.parseColor("#ffffff"))
         progressStartColor = attributes.getColor(R.styleable.ProgressRing_cr_progressbar_start_color, Color.YELLOW)
         progressEndColor = attributes.getColor(R.styleable.ProgressRing_cr_progressbar_end_color, Color.BLUE)
         startAngle = attributes.getInteger(R.styleable.ProgressRing_cr_start_angle, 150)
@@ -75,64 +79,75 @@ class ProgressRing(context: Context, attrs: AttributeSet, defStyleAttr: Int = 0)
 
     private fun initPaint() {
         mBgPaint = Paint()
-        mBgPaint.color = bgEndColor
+        mBgPaint.strokeCap = Paint.Cap.ROUND
         mBgPaint.strokeWidth = progressbarWidth.toFloat()
         mBgPaint.isAntiAlias = true
         mBgPaint.style = Paint.Style.STROKE
+        val mShader = SweepGradient(0f, 0f, intArrayOf(bgStartColor, bgMidColor, bgEndColor), floatArrayOf(0f, sweepAngle / 2f / 360, sweepAngle / 360f))
+        mBgPaint.shader = mShader
 
         mProgressPaint = Paint()
         mProgressPaint.strokeCap = Paint.Cap.ROUND
-        mBgPaint.isAntiAlias = true
-        mProgressPaint.color = progressEndColor
+        mProgressPaint.isAntiAlias = true
         mProgressPaint.strokeWidth = progressbarWidth.toFloat()
         mProgressPaint.style = Paint.Style.STROKE
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        if (!isAnimShow)
+            currentProgress = progress
+        canvas.save();//保存当前画布状态
+        canvas.translate(width / 2f, width / 2f); //将坐标中心平移到要围绕的坐标点x,y
+        canvas.rotate(startAngle.toFloat());//旋转角度
+
         drawBackground(canvas)
         drawProgress(canvas)
-    }
 
-    private fun drawBackground(canvas: Canvas) {
-        val progressAngle = sweepAngle * progress / 100
+        canvas.restore();//恢复画图状态到保存前
 
-        val mRectF = getRoundRect(progressbarWidth)
-        for (it in 0..sweepAngle - progressAngle - 1) {
-            if (it == 0 || it == sweepAngle - progressAngle - 1)
-                mBgPaint.strokeCap = Paint.Cap.ROUND
-            else
-                mBgPaint.strokeCap = Paint.Cap.BUTT
-
-            mBgPaint.color = getGradient(it / (sweepAngle - progressAngle.toFloat()), Color.parseColor("#33333333"), bgEndColor)
-            canvas.drawArc(mRectF, it + startAngle + progressAngle.toFloat(), 1f, false, mBgPaint)
+        if (currentProgress < progress) {
+            currentProgress++
+            postInvalidate()
+        } else if (currentProgress > progress) {
+            currentProgress--
+            postInvalidate()
         }
     }
 
-    private fun drawProgress(canvas: Canvas) {
-        val progressAngle = sweepAngle * progress / 100
-
+    private fun drawBackground(canvas: Canvas) {
+        val progressAngle = sweepAngle * currentProgress / 100
         val mRectF = getRoundRect(progressbarWidth)
-        canvas.drawArc(mRectF, startAngle.toFloat(), progressAngle.toFloat(), false, mProgressPaint)
+
+        canvas.drawArc(mRectF, progressAngle.toFloat(), sweepAngle.toFloat() - progressAngle.toFloat(), false, mBgPaint)
+
     }
 
-    //以宽为直径
+    private fun drawProgress(canvas: Canvas) {
+        val progressAngle = sweepAngle * currentProgress / 100
+        progressAngle.toString().log("progress")
+
+        val mShader = SweepGradient(0f, 0f, intArrayOf(progressStartColor, progressEndColor, progressStartColor), floatArrayOf(0f, progressAngle / 360f, 1.1f))
+        mProgressPaint.shader = null
+        mProgressPaint.shader = mShader
+        val mRectF = getRoundRect(progressbarWidth)
+
+        canvas.drawArc(mRectF, 0f, progressAngle.toFloat(), false, mProgressPaint)
+    }
+
     private fun getRoundRect(barWidth: Int): RectF {
         val mRectF = RectF()
-        mRectF.left = (barWidth / 2).toFloat() // 左上角x
-        mRectF.top = (barWidth / 2).toFloat() // 左上角y
-        mRectF.right = (width - barWidth / 2).toFloat() // 左下角x
-        mRectF.bottom = (width - barWidth / 2).toFloat() // 右下角y
+        mRectF.left = barWidth / 2.toFloat() - width / 2 // 左上角x
+        mRectF.top = barWidth / 2.toFloat() - width / 2 // 左上角y
+        mRectF.right = width / 2 - barWidth / 2f // 左下角x
+        mRectF.bottom = width / 2 - barWidth / 2f // 右下角y
         return mRectF
     }
 
     /**
      * 修改颜色透明度
-
      * @param color
-     * *
      * @param alpha
-     * *
      * @return
      */
     fun getGradient(fraction: Float, startColor: Int, endColor: Int): Int {
--- demo/src/main/java/top/guuguo/myapplication/ProgressActivity.kt
+++ demo/src/main/java/top/guuguo/myapplication/ProgressActivity.kt
@@ -3,16 +3,20 @@ package top.guuguo.myapplication
 import android.app.Activity
 import android.content.Intent
 import com.guuguo.android.lib.app.LNBaseActivity
+import kotlinx.android.synthetic.main.activity_progress.*
 
 class ProgressActivity : LNBaseActivity() {
     override fun getLayoutResId() = R.layout.activity_progress
     override fun initView() {
         super.initView()
+        pr_progress.setOnClickListener {
+            pr_progress.progress = (Math.random() * 100).toInt()
+        }
     }
 
     companion object {
         fun intentTo(activity: Activity) {
-            val intent= Intent(activity,ProgressActivity::class.java)
+            val intent = Intent(activity, ProgressActivity::class.java)
             activity.startActivity(intent)
         }
     }
